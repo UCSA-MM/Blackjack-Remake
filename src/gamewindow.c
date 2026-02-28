d99eb68a00885d35e102aad3188911f557f38ede
matte <UCSA>
2026-02-28 14:19:23 +0100

    added logic for the first cards to be drawn

--- src/gamewindow.c
+++ src/gamewindow.c
@@ -9,6 +9,23 @@
 //#include "blackjackclient.h"
 #include "gamewindow.h"
 
+//game options are in the header file
+#define MAX_CARD_NUM 5
+#define CARD_WIDTH 0.156f
+#define CARD_X_START 0.03f
+#define CARD_X_STEP 0.171f
+#define DEALER_CARD_Y 0.1f
+#define PLAYER_CARD_Y 0.55f
+#define CARD_HEIGHT 0.35f
+
+void game_DrawCard(card card, Rectangle target);
+
+Rectangle arr_recPlayerCards[MAX_CARD_NUM] = {0};
+Rectangle arr_recDealerCards[MAX_CARD_NUM] = {0};
+
+float screenWidth = 0.f;
+float screenHeight = 0.f;
+
 void GameStart(bool is_logged_in) {
   deck gameDeck = FillDeck(DECK_NUM);
   ShuffleDeck(&gameDeck);
@@ -25,13 +42,26 @@ void GameStart(bool is_logged_in) {
   int playerHandScore = CalcScore(playerHand, playerCardNum);
   int dealerCardScore = CalcScore(dealerHand, dealerCardNum);
 
-  //start of program
-  if(!IsWindowState(FLAG_BORDERLESS_WINDOWED_MODE)) {
-    SetWindowState(FLAG_BORDERLESS_WINDOWED_MODE);
+  screenWidth = (float)GetScreenWidth();
+  screenHeight = (float)GetScreenHeight();
+
+  for (int i = 0; i < MAX_CARD_NUM; i++) {
+    arr_recPlayerCards[i].x = (CARD_X_START + (i * CARD_X_STEP)) * screenWidth;
+    arr_recDealerCards[i].x = arr_recPlayerCards[i].x;
+    arr_recPlayerCards[i].width = screenWidth * CARD_WIDTH;
+    arr_recDealerCards[i].width = arr_recPlayerCards[i].width;
+    arr_recPlayerCards[i].y = screenHeight * PLAYER_CARD_Y;
+    arr_recDealerCards[i].y = screenHeight * DEALER_CARD_Y;
+    arr_recPlayerCards[i].height = screenHeight * CARD_HEIGHT;
+    arr_recDealerCards[i].height = arr_recPlayerCards[i].height;
   }
 
-  float screenWidth = (float)GetScreenWidth();
-  float screenHeight = (float)GetScreenHeight();
+  //start of program
+  
+  //borderless creates issues on WSL since the sizes get fucked up, will add a #IFDEF or something later
+  //if(!IsWindowState(FLAG_BORDERLESS_WINDOWED_MODE)) {
+    //SetWindowState(FLAG_BORDERLESS_WINDOWED_MODE);
+  //}
 
   while (!WindowShouldClose()) {
 
@@ -39,10 +69,22 @@ void GameStart(bool is_logged_in) {
 
     ClearBackground(DARKGREEN);
 
+    for (int i = 0; i < playerCardNum; i++) {
+      game_DrawCard(playerHand[i], arr_recPlayerCards[i]);
+    }
+    for (int i = playerCardNum; i < MAX_CARD_NUM; i++) {
+      DrawRectangleRec(arr_recPlayerCards[i], GREEN);
+    }
+
     EndDrawing();
   }
 
   free(gameDeck.cards);
   exit(0);
     
+}
+
+void game_DrawCard(card card, Rectangle target) {
+  //card text to be implemented
+  DrawRectangleRec(target, RAYWHITE);
 }
